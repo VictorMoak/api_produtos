30d5b5ba86f23b13a2d24484c28ca969b7f62914
Victor Moak <vmoak10@gmail.com>
2021-07-25 12:51:11 -0300

    teste rota

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ const ProdutosController = require('./app/Controllers/ProdutosController');
 const routes = new Router();
 
 routes.post("/api/addProdutos", ProdutosController.add);
-routes.get("/", ProdutosController.index);
-routes.get("/api/produtos", ProdutosController.show);
+//routes.get("/", ProdutosController.index);
+//routes.get("/api/produtos", ProdutosController.show);
 
 module.exports = routes;
