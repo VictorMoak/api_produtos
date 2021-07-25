908510dcca0be02792132537f8aaa8938d7ee2c9
Victor Moak <vmoak10@gmail.com>
2021-07-25 17:03:13 -0300

    tudo lindo na bahia

--- src/app/Controllers/ProdutosController.js
+++ src/app/Controllers/ProdutosController.js
@@ -20,6 +20,34 @@ class ProdutosController {
     show(req, res) {
         return res.json(db)
     }
+
+    details(req, res) {
+        const desc = req.params.desc
+        const details = db.filter(db => (db.descricao == desc))
+        res.json(details)
+    }
+
+    delete(req, res) {
+        const desc = req.params.desc
+
+        const index = db.findIndex(db => (db.descricao == desc))
+
+        db.splice(index, 1)
+
+        return res.status(200).end()
+    }
+
+    update(req, res) {
+        const body = req.body
+
+        const desc = req.params.desc
+
+        const index = db.findIndex(db => (db.descricao == desc))
+
+        db.splice(index, 1, body)
+
+        return res.status(200).end()
+    }
 }
 
 module.exports = new ProdutosController();
--- src/routes.js
+++ src/routes.js
@@ -7,5 +7,9 @@ const routes = new Router();
 routes.post("/api/addProdutos", ProdutosController.add);
 routes.get("/", ProdutosController.index);
 routes.get("/api/produtos", ProdutosController.show);
+routes.delete("/api/produtos-delete/:desc", ProdutosController.delete);
+routes.get("/api/produtos-details/:desc", ProdutosController.details);
+routes.put("/api/produtos-update/:desc", ProdutosController.update);
+
 
 module.exports = routes;
